8cb06080dee1f7dd90c3e255e20f96a256218c8f
SatBoy15 <albiphone2.0@gmail.com>
2017-06-15 19:00:35 +0200

    Update OPUSPlaylistScriptV2.0.py

--- OPUSPlaylistScriptV2.0.py
+++ OPUSPlaylistScriptV2.0.py
@@ -1,5 +1,8 @@
 import urllib, urllib2, string, base64, datetime, os, json, shutil, time, zipfile, re, stat, sys
 from random import randint
+
+originweburl = "YUhSMGNEb3ZMM1IyYldGemRHVnlMbWh2YkM1bGN5OXNhWE4wWlM5dmNIVnpMWFJsYzNRdE15NTBlSFE9"
+
 print "Playlist Wilmaa and Wilmaa-OPUS by Albiii!"
 
 #Input the Quality of the Channels
@@ -23,25 +26,37 @@ else:
       bit = '394000'
       qual2='01'
 
-#Script OPUS-Token
-
-print "Getting OPUS-Token..."
-linktok1 = urllib.urlopen("http://racacaxtv.ga/mega.php?chn=VEYx&pls=RnJhbmNvcGhvbmVz")
-pagetok1 = linktok1.read()
-postoken11 = string.find(pagetok1,"wil/4BHu")
-postoken21 = string.find(pagetok1,"/tf_1_hd/")
-tokenopuscf = pagetok1[(postoken11+4):(postoken21)]
-
-
-print "Got Tokens and Links, starting step 2..."
+#Reading Origin File
 
 print "Reading origin file and copying it..."
-originweb = urllib.urlopen("http://tvmaster.hol.es/liste/opus-final-1.txt")
+originweb = urllib.urlopen(base64.b64decode(base64.b64decode(originweburl)))
 dest = originweb.read()
 
 print "Copied."
 
 originweb.close()
+
+
+#Script OPUS-Token
+
+def OpusCFToken(tipo):
+    if tipo == "base": 
+        print "Getting OPUS-Token V2..."
+        linktok1 = urllib.urlopen("http://racacaxtv.ga/mega.php?chn=VEYx&pls=RnJhbmNvcGhvbmVz")
+        pagetok1 = linktok1.read()
+        postoken11 = string.find(pagetok1,"wil/4BHu")
+        postoken21 = string.find(pagetok1,"/tf_1_hd/")
+        tokenopuscf = pagetok1[(postoken11+4):(postoken21)]
+        return tokenopuscf
+    elif tipo == "sha":
+        linktok1 = urllib.urlopen("http://opus.cf/allfrtvstrm/tkn/hd1/tok/i/weblive_1@90467/index_1240_av-b.m3u8")
+        pagetok1 = link1.read()
+        tokenopuscf = pagetok1[57:97]
+        return tokenopuscf
+    else:
+        pass
+
+tokenopuscf = OpusCFToken('base')
 
 #MAIN FUNCTIONS
 #Opus Session Script 
@@ -91,51 +106,102 @@ def FranceTVlink(ident,quality):
     stream = "http://www.mytvonline.org/minproxy.php/http://live.francetv.fr/simulcast/France_"+ident+"/hls_v1/" + tokfra + "/France_"+ident+"-video=1465200-audio_AACL_fra_65600_"+quality+"=65600.m3u8" + tokfrb
     return stream
 
-#Choose France Stream
+# TEST Selection
+ 
+def Selection(index,lastchid,dest):
+    lastchid = str(lastchid)
+    posindex = 0
+    pos8link = 0
+    chidfin = ""
+    posindexinfo = index+'@'
+    u8link2 = '.m3u8link'
+    posind2 = len(posindexinfo)+1
+    while chidfin != lastchid:
+        posindex = dest.find(posindexinfo, posindex + 1)
+        print "Channel ID Start: "+str(posindex)
+        pos8link = dest.find(u8link2, posindex + 1)
+        print "Channel ID finish: "+str(pos8link)
+        if posindexinfo == "francetvch@":
+            chidfin = dest[(posindex+posind2)-1:(posindex+posind2)]
+            print "             France "+chidfin
+            finlink1 = FranceTVlink(chidfin,"")
+        elif posindexinfo == "chan@":
+            chidfin = dest[(posindex+posind2)-1:pos8link]
+            print "             "+chidfin
+            finlink1 = OpusProxyLink(chidfin,qual)
+        dest = dest.replace(posindexinfo+chidfin+u8link2,finlink1+".m3u8")
+        print "OK, done!"
+    else:
+        return dest
 
-posfr = 0
-pos8link = 0
-lastchid = "O"
-ident = ""
-while ident != lastchid:
-    posfr = dest.find('francetvch@', posfr + 1)
-    pos8link = posfr
-    pos8link = dest.find('.m3u8link', pos8link + 1)
-    ident = dest[posfr+11:pos8link]
-    print "France " + ident+ "..."
-    finlink1 = FranceTVlink(ident,quality)
-    dest = dest.replace("francetvch@"+ident+".m3u8link",finlink1+".m3u8")
-    print "OK"
+#Script OPUS RTS channels
+
+def RTSCh(dest):
+    chopus1 = OpusProxyLink("orf_2_hd",qual)
+    chopus2 = chopus1
+    print 'Setting up RTS Un...'
+    chopus1 = string.replace(chopus1,"/1/1011/","/2/2034/")
+    chopus1 = string.replace(chopus1,"index","index.m3u8")
+    dest = string.replace(dest,"chopus1",chopus1)
+    print 'OK, now setting RTS Deux...'
+    chopus2 = string.replace(chopus2,"/1/1011/","/2/2035/")
+    chopus2 = string.replace(chopus2,"index","index.m3u8")
+    dest = string.replace(dest,"chopus2",chopus2)
+    print 'RTS channels OK...'
+    return dest
+
+#Script RTBF
+
+print "Starting RTBF Channels Token finder..."
+def RTBF(opusurlid):
+    opusurlid = base64.b64encode(opusurlid)
+    linktokrtbf = urllib.urlopen("http://racacaxtv.ga/mega.php?chn="+opusurlid+"&pls=RnJhbmNvcGhvbmVz")
+    pagetokrtbf = linktokrtbf.read()
+    postokenbel1 = string.find(pagetokrtbf,"/open/")
+    postokenbel2 = string.find(pagetokrtbf,"/la")
+    tokenbel1=pagetokrtbf[postokenbel1+6:postokenbel2]
+    return tokenbel1
+
+#FrenchStream
+    
+indexfra = "francetvch"
+lastfra = "O"
+dest = Selection(indexfra,lastfra,dest)
+
+#Wilmaa Streams
+
+indexwil = "chan"
+lastwil = "orf_2_hd"
+dest = Selection(indexwil,lastwil,dest)
+
+#RTBF La Une
+print "Finding RTBF La Une Token and replacing it..."
+
+rtbftoken = RTBF("RTBF La Une")
+dest = dest.replace("launetok",rtbftoken)
+
+#RTBF La Deux
+print "Finding RTBF La Deux Token and replacing it..."
+
+rtbftoken = RTBF("RTBF La Deux")
+dest = dest.replace("ladeuxtok",rtbftoken)
+
+#RTBF La Trois
+print "Finding RTBF La Trois Token and replacing it..."
+
+rtbftoken = RTBF("RTBF La Trois")
+dest = dest.replace("latroistok",rtbftoken)
+
+print "OK, RTBF Done!"
 
 #RTS suisse channels on Opus
 
-chopus0 = OpusProxyLink("orf_2_hd",qual)
-chopus1 = chopus0
-chopus2 = chopus0
-print 'Setting up RTS Un...'
-chopus1 = string.replace(chopus1,"/1/1011/","/2/2034/")
-chopus1 = string.replace(chopus1,"index","index.m3u8")
-print 'OK, now setting RTS Deux...'
-chopus2 = string.replace(chopus2,"/1/1011/","/2/2035/")
-chopus2 = string.replace(chopus2,"index","index.m3u8")
-print 'RTS channels OK...'
+dest = RTSCh(dest)
 
-#Script Change OPUS channels in m3u playlist
-posopus = 0
-poshd = 0
-lastchid = "orf_2_hd"
-chid = ""
-while chid != "orf_2_hd":
-    posopus = dest.find('chan@', posopus + 1)
-    poshd = posopus
-    poshd = dest.find('.m3u8link', poshd + 1)
-    chid = dest[posopus+5:poshd]
-    print chid
-    finlink2 = OpusProxyLink(chid,qual)
-    dest = dest.replace("chan@"+chid+".m3u8link",finlink2+".m3u8")
+#Step 2.
 
 #Copying All in the final file.
-
+print "Got Tokens and Links, starting step 2..."
 print "Opening Copy and replacing strings..."
 dest1 = file("play-weborigin-streams.m3u","w")
 data = time.strftime("%d/%m")
@@ -143,8 +209,6 @@ dest = string.replace(dest,"datareplace",data)
 dest = string.replace(dest,"tokenchange",tokenopuscf)
 dest = string.replace(dest,"qual2",qual2)
 dest = string.replace(dest,"qual",qual)
-dest = string.replace(dest,"chopus1",chopus1)
-dest = string.replace(dest,"chopus2",chopus2)
 
 dest1.write(dest)
 
@@ -153,5 +217,3 @@ print "Writing last things..."
 dest1.close()
 
 print "OK, all done!"
-
-#os.system("pause")
